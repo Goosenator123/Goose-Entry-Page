5f582a30267e71e486f08ba82f3493733991f750
Simon Cao <simon.cao2007@gmail.com>
2023-10-21 20:50:20 -0400

    Polished the codes Implemented a Cooldown on light/dark modes

--- backend/darkMode_lightMode.js
+++ backend/darkMode_lightMode.js
@@ -3,12 +3,24 @@ const themeOptions = {
     'dark': false,
     'light': false,
 };
+let isCooldown = false;
+const cooldownTimer = 1000;
 
 function toggleTheme(theme){
     document.body.classList.toggle(theme);
     themeOptions[theme] = !themeOptions[theme]
-};
+}
+
+function activateButton(){
+    isCooldown = false;
+}
 
 function executeChange(){
-    toggleTheme(themeOptions.dark ? 'light':'dark')
+    if(!isCooldown){
+        toggleTheme(themeOptions.dark ? 'light':'dark')
+        isCooldown = true;
+        setTimeout(activateButton, cooldownTimer);
+    }
 }
+
+changeTheme.addEventListener("click", executeChange)
