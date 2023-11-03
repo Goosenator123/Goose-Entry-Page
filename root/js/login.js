76d8f72ddd5ed2bb18f9f672b735c24ffea181a0
Simon Cao <simon.cao2007@gmail.com>
2023-11-03 00:04:22 -0400

    Made a first page for the entry page Added 7 websites and multiple videos for the first page Made a very scuffed second page for the easter eggs Added a forward/return button

--- root/js/login.js
+++ root/js/login.js
@@ -1,70 +1,90 @@
 // Get references to the HTML elements
-const signIn = document.getElementById("signInButton");
-const redirect = document.getElementById("redirectButton");
-const username = document.getElementById('username');
-const password = document.getElementById('password');
+const signIn = document.getElementById("signInButton"); // Sign In button
+const redirect = document.getElementById("redirectButton"); // Redirect button
+const usernameInput = document.getElementById('username'); // Username input field
+const passwordInput = document.getElementById('password'); // Password input field
+
+// Easter Eggs
+const rickroll = ['rick astley', 'rick', 'rickroll', 'never gonna give you up', 'never gonna give u up'];
 
 // Get references to error messages for validation
-const errorInvalid = document.getElementById("errorInvalid");
+const errorInvalid = document.getElementById("errorInvalid"); // Error message for invalid login
 
-// Function that checks if account exists
+// Function that checks if an account exists in local storage
 const checkExist = (username) => {
     const storedData = localStorage.getItem(username) == null ? true : false;
-    return storedData
+    return storedData;
 }
 
-// Function that checks if username and password are valid
+// Function that checks if the provided username and password are valid
 const checkAccount = (username, password) => {
-    const storedData = localStorage.getItem(username)
-    const existingData = JSON.parse(storedData)
+    const storedData = localStorage.getItem(username);
+    const existingData = JSON.parse(storedData);
 
-    if (existingData.username === username && existingData.password === password){
-        return false
+    if (existingData && existingData.username === username && existingData.password === password) {
+        return false; // Valid username and password
     } else {
-        return true
+        return true; // Invalid username or password
     }
 }
 
-// Redirect function to sign up page
-function redirecting() {
-    window.location.href = "signUpPage.html";
-}
-
-// Log in with Enter function
+// Log in with Enter key function
 function loginWithEnter(e) {
-    if(e.key == "Enter"){
-        login()
+    if (e.key == "Enter") {
+        login();
     }
 }
 
 // Log in Function
 function login() {
+    // Get inputed username and password
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Set error messages as hidden upon loaded
+    // Set error messages as hidden upon page load
     errorInvalid.style.visibility = "hidden";
 
-    // Error message if account does not exist
-    
-    if(username.length === 0 || password.length === 0){
-        errorInvalid.style.visibility = "visible";
-        errorInvalid.textContent = "Please fill out the required fields";
-    } else if(checkExist(username) || checkAccount(username, password)){
-        errorInvalid.style.visibility = "visible";
-        errorInvalid.textContent = "Invalid username or password";
+    // Check easter egg name
+    if (rickroll.includes(username.toLowerCase())) {
+        for (const name of rickroll) {
+            if (name.toLowerCase() === username.toLowerCase()) {
+                // Display successful EASTER EGG finding
+                errorInvalid.style.visibility = "visible";
+                errorInvalid.style.color = "green";
+                errorInvalid.textContent = `EASTER EGG BABYYYYY!!!`;
+                errorInvalid.style.fontWeight = "bold";
+                setTimeout(() => {
+                    window.location.href = "https://youtu.be/xvFZjo5PgG0?si=jb_XYw-aF93jrbiT";
+                }, 1500);                
+            }
+         }
     } else {
-        errorInvalid.style.visibility = "visible";
-        errorInvalid.style.color = "green"
-        errorInvalid.textContent = `${username} is logged in`;
-        setTimeout(() => {
-            window.location.href = "firstPage.html";
-        }, 2000)
+
+        // Display error message if the account does not exist
+        if (username.length === 0 || password.length === 0) {
+            errorInvalid.style.visibility = "visible";
+            errorInvalid.textContent = "Please fill out the required fields";
+        } else if (checkExist(username) || checkAccount(username, password)) {
+            errorInvalid.style.visibility = "visible";
+            errorInvalid.textContent = "Invalid username or password";
+        } else {
+            // Display successful logged in message
+            errorInvalid.style.visibility = "visible";
+            errorInvalid.style.color = "green";
+            errorInvalid.textContent = `Logging ${username} in`;
+
+            // Redirect to the first page after a brief delay
+            setTimeout(() => {
+                window.location.href = "firstPage.html";
+            }, 1500);
+        }
     }
 }
 
-// randomBullshit.exe
-signIn.addEventListener("click", login);
-username.addEventListener("keydown", loginWithEnter);
-password.addEventListener("keydown", loginWithEnter);
-redirect.addEventListener("click", redirecting);
+// Event listeners
+signIn.addEventListener("click", login); // Trigger login function when Sign In button is clicked
+usernameInput.addEventListener("keydown", loginWithEnter); // Trigger loginWithEnter function when Enter key is pressed in the username field
+passwordInput.addEventListener("keydown", loginWithEnter); // Trigger loginWithEnter function when Enter key is pressed in the password field
+redirect.addEventListener("click", () => {
+    window.location.href = "signUpPage.html";
+}); // Trigger redirecting function when Redirect button is clicked
--- root/js/register.js
+++ root/js/register.js
@@ -1,14 +1,19 @@
 // Get references to the HTML elements
-const signUp = document.getElementById("signUpButton");
-const signInForm = document.getElementById("signInForm");
-const redirectButton = document.getElementById("redirectButton");
+const signUp = document.getElementById("signUpButton"); // Sign Up button
+const signInForm = document.getElementById("signInForm"); // Sign-in form
+const redirectButton = document.getElementById("redirectButton"); // Redirect button
+const confirmPassword = document.getElementById("confirm-password"); // Confirm Password input field
+const usernameInput = document.getElementById('username'); // Username input field
+const passwordInput = document.getElementById('password'); // Password input field
+const success = document.getElementById("successfulSignIn") // Sucessful creation message
 
 // Get references to error messages for validation
-const errorUserLength = document.getElementById("errorUserLength");
-const errorUserTaken = document.getElementById("errorUserTaken");
-const errorPswLength = document.getElementById("errorPswLength");
-const errorPswUpper = document.getElementById("errorPswUpper");
-const errorConfirmation = document.getElementById("errorConfirmation");
+const errorUser = document.getElementById("errorUser"); // Error message for username
+const errorPsw = document.getElementById("errorPsw"); // Error message for password
+const errorConfirmation = document.getElementById("errorConfirmation"); // Error message for password confirmation
+
+// Easter Eggs
+const rickroll = ['rick astley', 'rick', 'rickroll', 'never gonna give you up', 'never gonna give u up'];
 
 // Function to check if a password contains special characters
 function containsSpecialCharacter(password) {
@@ -22,16 +27,11 @@ function containsUpperCase(password) {
     return upperCases.test(password);
 }
 
-// Function to redirect to another page
-function redirecting() {
-    window.location.href = "index.html";
-}
-
-// Prevent the Enter key to submit form early
-function preventSubmission(e){
+// Prevent the Enter key from submitting the form prematurely
+function registerWithEnter(e){
     if(e.key == "Enter"){
-        register(e)
-        e.preventDefault();
+        register(e); // Call the registration function
+        e.preventDefault(); // Prevent the default form submission behavior
     }
 }
 
@@ -50,70 +50,105 @@ const checkUsername = (username) => {
 // Function to handle user registration
 function register(event) {
     event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get inputed information
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
 
-    // Validation checks for the registration form
-    if (
-        username.length < 6 || // Username should be at least 6 characters long
-        password.length < 6 || // Password should be at least 6 characters long
-        confirmPassword !== password || // Password and confirmation do not match
-        checkUsername(username) === true || // Username is already taken
-        !containsSpecialCharacter(password) || // Password does not contain special characters
-        !containsUpperCase(password) // Password does not contain uppercase letters
-    ) {
-        errorUserLength.style.visibility = "hidden";
-        errorPswLength.style.visibility = "hidden";
-        errorPswUpper.style.visibility = "hidden";
-        errorConfirmation.style.visibility = "hidden";
+    // Hide existing error messages
+    errorUser.style.visibility = "hidden";
+    errorPsw.style.visibility = "hidden";
+    errorConfirmation.style.visibility = "hidden";
 
-        // Error messages for invalid usernames 
-        if (username.length === 0) {
-            errorUserLength.style.visibility = "visible";
-            errorUserLength.textContent = "Please enter a username";
-        } else if (username.length < 6) {
-            errorUserLength.style.visibility = "visible";
-            errorUserLength.textContent = "Username must be at least 6 characters long";
-        } else if (checkUsername(username)) {
-            errorUserTaken.style.visibility = "visible";
-            errorUserTaken.textContent = "The username is already taken";
-            let data = localStorage.getItem(username);
-            console.log('The existing account is ' + data);
-        } else {
-
-            // Error messages for invalid passwords
-            if (password.length === 0) {
-                errorPswLength.style.visibility = "visible";
-                errorPswLength.textContent = "Please enter a password";
-            } else if (password.length < 6) {
-                errorPswLength.style.visibility = "visible";
-                errorPswLength.textContent = "Password must be at least 6 characters long";
-            } else if (!containsSpecialCharacter(password) || !containsUpperCase(password)) {
-                errorPswLength.style.visibility = "visible";
-                errorPswLength.textContent = "Password must contain a symbol and an uppercase letter";
-            } else {
-                if (confirmPassword !== password) {
-                    errorConfirmation.style.visibility = "visible";
-                    errorConfirmation.textContent = "Your passwords do not match";
-                }
+    // Check easter egg name
+    if (rickroll.includes(username.toLowerCase())) {
+        for (const name of rickroll) {
+            if (name.toLowerCase() === username.toLowerCase()) {
+                // Display successful sign up message
+                success.style.visibility = "visible";
+                success.style.color = "green";
+                success.style.fontWeight = "bold";
+                success.textContent = `EASTER EGGG?????`;
+                setTimeout(() => {
+                    window.location.href = "https://youtu.be/xvFZjo5PgG0?si=jb_XYw-aF93jrbiT";
+                }, 1500);
             }
         }
     } else {
-        // If all checks pass, save the user data to localStorage and redirect to another page
-        const userData = {
-            username: username,
-            password: password,
-        };
-        localStorage.setItem(username, JSON.stringify(userData));
+        // Validation checks for the registration form
+        if (
+            username.length < 6 || // Check if username is at least 6 characters long
+            password.length < 6 || // Check if password is at least 6 characters long
+            confirmPassword !== password || // Check if password and confirmation do not match
+            checkUsername(username) === true || // Check if username is already taken
+            !containsSpecialCharacter(password) || // Check if password does not contain special characters
+            !containsUpperCase(password) // Check if password does not contain uppercase letters
+        ) {
 
-        // Redirecting after submission
-        redirecting();
-    }
+            // Error messages for invalid usernames 
+            if (username.length === 0) {
+                errorUser.style.visibility = "visible";
+                errorUser.textContent = "Please enter a username";
+            } else if (username.length < 6) {
+                errorUser.style.visibility = "visible";
+                errorUser.textContent = "Username must be at least 6 characters long";
+            } else if (checkUsername(username)) {
+                errorUser.style.visibility = "visible";
+                errorUser.textContent = "The username is already taken";
+                let data = localStorage.getItem(username);
+                console.log('The existing account is ' + data);
+            } else {
+                // Error messages for invalid passwords
+                if (password.length === 0) {
+                    errorPsw.style.visibility = "visible";
+                    errorPsw.textContent = "Please enter a password";
+                } else if (password.length < 6) {
+                    errorPsw.style.visibility = "visible";
+                    errorPsw.textContent = "Password must be at least 6 characters long";
+                } else if (!containsSpecialCharacter(password) || !containsUpperCase(password)) {
+                    errorPsw.style.visibility = "visible";
+                    errorPsw.textContent = "Password must contain a symbol and an uppercase letter";
+                } else {
+                    if (confirmPassword !== password) {
+                        errorConfirmation.style.visibility = "visible";
+                        errorConfirmation.textContent = "Your passwords do not match";
+                    }
+                }
+            }
+        } else {
+            // Display successful sign up message
+            success.style.visibility = "visible";
+            success.style.color = "green";
+            success.textContent = `${username} account has been created`;
+
+            // If all checks pass, save the user data to localStorage and redirect to another page
+            const userData = {
+                username: username,
+                password: password,
+            };
+
+            // If statement that is purely for testing
+            if (username === "testing") {
+                console.log(`${username} has been deleted!`)
+            } else {
+                // Store the created username with its password
+                localStorage.setItem(username, JSON.stringify(userData));
+            }
+            
+            // Redirect after successful registration
+            setTimeout(() => {
+                window.location.href = "firstPage.html";
+            }, 1500);     
+        }  
+    } 
 }
 
-// Attach a click event listener to the signup button to trigger the registration process
-signUp.addEventListener("click", register);
-document.addEventListener("keydown", preventSubmission)
-// Attach a click event listener to the redirect button to trigger the redirection
-redirectButton.addEventListener("click", redirecting);
+// Event listeners
+signUp.addEventListener("click", register); // Trigger the registration process when Sign Up button is clicked
+usernameInput.addEventListener("keydown", registerWithEnter); // Trigger registrationWithEnter function when a key is pressed in the username field
+passwordInput.addEventListener("keydown", registerWithEnter); // Trigger registrationWithEnter function when a key is pressed in the password field
+confirmPassword.addEventListener("keydown", registerWithEnter); // Trigger registrationWithEnter function when a key is pressed in the confirm password field
+redirectButton.addEventListener("click", () => {
+    window.location.href = "index.html";
+}); // Trigger redirection when Redirect button is clicked
